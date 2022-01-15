e81e37e93889917d7750489ad766b3133f6a1d94
bentlyedyson <56628774+bentlyedyson@users.noreply.github.com>
2022-01-15 23:20:35 +0700

    model added
    3d, import, coded,. jpeg, png
    mesh

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,12 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader, ImportMeshAsync, PointLight, CreateScene } from "@babylonjs/core";
 
 class App {
+
   constructor() {
+      
       // create the canvas html element and attach it to the webpage
       const canvas = document.createElement("canvas");
       canvas.style.width = "100%";
@@ -16,19 +18,18 @@ class App {
       const engine = new Engine(canvas, true);
       const scene = new Scene(engine);
 
-      const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-      camera.attachControl(canvas, true);
-      const light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-    
-    //3d load 
-    BABYLON.SceneLoader.ImportMeshAsync("", "", "heart.babylon");
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(500, 500, 5000));
+        // load 3d model
+        SceneLoader.ImportMeshAsync("", "./", "heart.babylon", scene);
+
+        const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+
+        const light = new HemisphericLight("light", new Vector3(500, 500, 5000));
         light.intensity = 100;
-    
-        const light2 = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 0, -50), scene);
-        light2.intensity = 600000 ; 
-        scene.debugLayer.show();
-    
+
+        const light1 = new PointLight("light", new Vector3(0, 0, -50), scene);
+        light1.intensity = 600000 ; 
+
       // hide/show the Inspector
       window.addEventListener("keydown", (ev) => {
           // Shift+Ctrl+Alt+I
