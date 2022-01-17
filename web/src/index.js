84841de694d302d0e27687cc9e03d6e05c6cd9a7
bentlyedyson <56628774+bentlyedyson@users.noreply.github.com>
2022-01-17 09:52:33 +0700

    animation and non animatoin added

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,8 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader, ImportMeshAsync, PointLight, CreateScene } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader, ImportMeshAsync, PointLight, CreateScene, Button, CreateSimpleButton} from "@babylonjs/core";
+import { bayerDitherFunctions } from "@babylonjs/core/Shaders/ShadersInclude/bayerDitherFunctions";
 
 class App {
 
@@ -18,19 +19,87 @@ class App {
       const engine = new Engine(canvas, true);
       const scene = new Scene(engine);
 
-        // load 3d model
-        SceneLoader.ImportMeshAsync("", "./", "heart.babylon", scene);
 
+        // load 3d model
+        function heart() {
+        const stat = SceneLoader.ImportMesh("", "./", "heart.babylon", scene)
+
+        const light3 = new HemisphericLight("light", new Vector3(500, 500, 5000), scene);
+        light3.intensity = 100;
+
+        const light4 = new PointLight("light", new Vector3(50, 50, -200), scene);
+        light4.intensity = 600000 ;  
+
+        const light5 = new PointLight("light", new Vector3(50, 50, -200), scene);
+        light5.intensity = 600000 ;  
+        var scale = true ;
+        }
+
+        function heart1(){ SceneLoader.ImportMesh("", "./", "heart_animation.babylon", scene, (meshes) => {
+            
+            const light = new HemisphericLight("light", new Vector3(500, 500, 5000), scene);
+            light.intensity = 100;
+    
+            const light1 = new PointLight("light", new Vector3(50, 50, -200), scene);
+            light1.intensity = 600000 ;  
+    
+            const light2 = new PointLight("light", new Vector3(50, 50, -200), scene);
+            light2.intensity = 600000 ;  
+            var scale = true ;
+
+            scene.registerBeforeRender(function () {
+                // Check if heart is moving 
+                
+                for (const mesh of meshes) {
+                    
+
+                    var x = 1.4
+                    var y = 1.4
+                    var z =1.4
+                    
+                    if (mesh.scaling.x < 1.5 && scale || mesh.scaling.y < 1.5 && scale || mesh.scaling.z < 1.5 && scale ) {
+                        // Increment heart scaling 
+                        mesh.scaling.x += 0.05;
+                        mesh.scaling.y += 0.05;
+                        mesh.scaling.z += 0.05;
+                    }
+            
+                    else {
+                        // Swap scales to decrease
+                        scale = false;
+                    }
+            
+        
+                    if (mesh.scaling.x > 0.8 && !scale || mesh.scaling.y > 0.8 && scale || mesh.scaling.z > 0.8  && scale ) {
+                        // Decrement heart scaling 
+                        mesh.scaling.x -= 0.008;
+                        mesh.scaling.y -= 0.008;
+                        mesh.scaling.z -= 0.008;
+            
+                    }
+                    else {
+                        // Swap directions to increase
+                        scale = true;
+                    }
+
+                } 
+
+               
+            });
+  
+        });
+    };
         const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-
-        const light = new HemisphericLight("light", new Vector3(500, 500, 5000));
-        light.intensity = 100;
-
-        const light1 = new PointLight("light", new Vector3(0, 0, -50), scene);
-        light1.intensity = 600000 ; 
-
-      // hide/show the Inspector
+        
+        var renderRunning = false
+        if(renderRunning){
+            heart();
+        } else {
+            heart1();
+    
+        }
+      // hide/show the Inspector    
       window.addEventListener("keydown", (ev) => {
           // Shift+Ctrl+Alt+I
           if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
