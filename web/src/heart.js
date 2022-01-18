318536b80a603fa8a8955f843e3a5caf548a3ed0
Jason Christian H <nakamarujc@gmail.com>
2022-01-18 11:28:50 +0700

    Added function to beat

---
+++ web/src/heart.js
@@ -0,0 +1,115 @@
+import {
+  HemisphericLight,
+  PointLight,
+  SceneLoader,
+  Vector3,
+} from "@babylonjs/core";
+
+class Heart {
+  constructor(scene) {
+    // Props
+    this.minScale = 1.43;
+    this.maxScale = 1.5;
+    this.scale = true;
+    this.beat = false; // Whether the heart should beat
+    this.scene = scene;
+    this.meshes;
+
+    SceneLoader.ImportMesh(
+      "",
+      "./",
+      "heart_animation.babylon",
+      scene,
+      (meshes) => {
+        // Set up lights
+        const light = new HemisphericLight(
+          "light",
+          new Vector3(500, 500, 5000),
+          scene
+        );
+        light.intensity = 100;
+
+        const light1 = new PointLight(
+          "light",
+          new Vector3(50, 50, -200),
+          scene
+        );
+        light1.intensity = 600000;
+
+        const light2 = new PointLight(
+          "light",
+          new Vector3(50, 50, -200),
+          scene
+        );
+        light2.intensity = 600000;
+
+        for (const mesh of meshes) {
+          mesh.scaling.x = this.minScale;
+          mesh.scaling.y = this.minScale;
+          mesh.scaling.z = this.minScale;
+        }
+
+        this.meshes = meshes;
+        scene.registerBeforeRender(this._registerBeat.bind(this));
+      }
+    );
+  }
+
+  _registerBeat() {
+    if (!this.beat) return;
+
+    // Register animation
+    const { minScale, maxScale, meshes, scale } = this;
+
+    const scaleUpInc = 0.008;
+    const scaleDownInc = 0.002;
+
+    for (const mesh of meshes) {
+      if (scale) {
+        if (
+          mesh.scaling.x < maxScale ||
+          mesh.scaling.y < maxScale ||
+          mesh.scaling.z < maxScale
+        ) {
+          // Increment heart scaling
+          mesh.scaling.x += scaleUpInc;
+          mesh.scaling.y += scaleUpInc;
+          mesh.scaling.z += scaleUpInc;
+        } else {
+          // Swap scales to decrease
+          this.scale = false;
+        }
+      } else {
+        if (
+          mesh.scaling.x > minScale ||
+          mesh.scaling.y > minScale ||
+          mesh.scaling.z > minScale
+        ) {
+          // Decrement heart scaling
+          mesh.scaling.x -= scaleDownInc;
+          mesh.scaling.y -= scaleDownInc;
+          mesh.scaling.z -= scaleDownInc;
+        } else {
+          // Swap directions to increase
+          this.scale = true;
+          this.beat = false;
+        }
+      }
+    }
+  }
+
+  beatHeart() {
+    const { minScale, meshes } = this;
+    if (!meshes) return; // Safeguard
+    this.beat = true;
+    this.scale = true;
+
+    for (const mesh of meshes) {
+      mesh.scaling.x = minScale;
+      mesh.scaling.y = minScale;
+      mesh.scaling.z = minScale;
+    }
+  }
+}
+
+export default Heart;
--- web/src/index.js
+++ web/src/index.js
@@ -1,22 +1,8 @@
+import { ArcRotateCamera, Engine, Scene, Vector3 } from "@babylonjs/core";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import "@babylonjs/loaders/glTF";
-import {
-  Engine,
-  Scene,
-  ArcRotateCamera,
-  Vector3,
-  HemisphericLight,
-  Mesh,
-  MeshBuilder,
-  SceneLoader,
-  ImportMeshAsync,
-  PointLight,
-  CreateScene,
-  Button,
-  CreateSimpleButton,
-} from "@babylonjs/core";
-import { bayerDitherFunctions } from "@babylonjs/core/Shaders/ShadersInclude/bayerDitherFunctions";
+
+import Heart from "./heart";
 
 class App {
   constructor() {
@@ -31,111 +17,17 @@ class App {
     const engine = new Engine(canvas, true);
     const scene = new Scene(engine);
 
-    // load 3d model
-    function heart() {
-      SceneLoader.ImportMesh("", "./", "heart.babylon", scene);
-
-      const light3 = new HemisphericLight(
-        "light",
-        new Vector3(500, 500, 5000),
-        scene
-      );
-      light3.intensity = 100;
-
-      const light4 = new PointLight("light", new Vector3(50, 50, -200), scene);
-      light4.intensity = 600000;
-
-      const light5 = new PointLight("light", new Vector3(50, 50, -200), scene);
-      light5.intensity = 600000;
-      var scale = true;
-    }
-
-    function heart1() {
-      SceneLoader.ImportMesh(
-        "",
-        "./",
-        "heart_animation.babylon",
-        scene,
-        (meshes) => {
-          const light = new HemisphericLight(
-            "light",
-            new Vector3(500, 500, 5000),
-            scene
-          );
-          light.intensity = 100;
-
-          const light1 = new PointLight(
-            "light",
-            new Vector3(50, 50, -200),
-            scene
-          );
-          light1.intensity = 600000;
-
-          const light2 = new PointLight(
-            "light",
-            new Vector3(50, 50, -200),
-            scene
-          );
-          light2.intensity = 600000;
-          var scale = true;
-
-          scene.registerBeforeRender(function () {
-            // Check if heart is moving
-
-            for (const mesh of meshes) {
-              var x = 1.4;
-              var y = 1.4;
-              var z = 1.4;
-
-              if (
-                (mesh.scaling.x < 1.5 && scale) ||
-                (mesh.scaling.y < 1.5 && scale) ||
-                (mesh.scaling.z < 1.5 && scale)
-              ) {
-                // Increment heart scaling
-                mesh.scaling.x += 0.05;
-                mesh.scaling.y += 0.05;
-                mesh.scaling.z += 0.05;
-              } else {
-                // Swap scales to decrease
-                scale = false;
-              }
-
-              if (
-                (mesh.scaling.x > 0.8 && !scale) ||
-                (mesh.scaling.y > 0.8 && scale) ||
-                (mesh.scaling.z > 0.8 && scale)
-              ) {
-                // Decrement heart scaling
-                mesh.scaling.x -= 0.008;
-                mesh.scaling.y -= 0.008;
-                mesh.scaling.z -= 0.008;
-              } else {
-                // Swap directions to increase
-                scale = true;
-              }
-            }
-          });
-        }
-      );
-    }
+    const heart = new Heart(scene);
 
     const camera = new ArcRotateCamera(
       "Camera",
+      Math.PI / 6,
       Math.PI / 2,
-      Math.PI / 2,
-      2,
+      500,
       Vector3.Zero(),
       scene
     );
     camera.attachControl(canvas, true);
-
-    var renderRunning = false;
-    if (renderRunning) {
-      heart();
-    } else {
-      heart1();
-    }
 
     // hide/show the Inspector
     window.addEventListener("keydown", (ev) => {
